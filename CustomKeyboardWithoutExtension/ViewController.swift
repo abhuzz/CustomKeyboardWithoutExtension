9b72705c5ddaff75e916e22e6f20221f8f829ba6
hara <hara.kazushi@po.ntts.co.jp>
2014-12-12 20:46:22 +0900

    added project files

---
+++ CustomKeyboardWithoutExtension/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CustomKeyboardWithoutExtension
+//
+//  Created by haranicle on 2014/12/11.
+//  Copyright (c) 2014年 haranicle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onButtonPushed(sender: AnyObject) {
+    }
+
+}
+
